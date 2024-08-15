48e793622a2c174b2ffb7cdcd392ea8c50e31c6e
fangxuling <1933226149@qq.com>
2024-08-15 18:04:06 +0800

    ✨ feat(change ):

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 import DefaultTheme from "vitepress/theme";
 import "./styles/custom.css";
 import "./styles/global.css";
-import Layout from "./components/Layout.vue";
+import Layout from "../components/theme/Layout.vue";
 
 export default { ...DefaultTheme, Layout };
