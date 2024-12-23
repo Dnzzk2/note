d0253553a5d66085abe46fb41636e8bc0cba59cc
Dnzzk2 <1933226149@qq.com>
2024-12-23 23:32:30 +0800

    🎈 perf(update):

--- docs/.vitepress/scripts/nav.ts
+++ docs/.vitepress/scripts/nav.ts
@@ -3,15 +3,11 @@ import type { DefaultTheme } from "vitepress";
 const nav: DefaultTheme.NavItem[] = [
   {
     text: "AI",
-    link: "/AI/cusror-prompt",
+    link: "/AI/cursor-prompt",
   },
   {
     text: "WorkFlow",
     link: "/workflow/vscode/setting",
-  },
-  {
-    text: "CSS",
-    link: "/Cascading-Style-Sheets/attributes/Flex和inline-flex",
   },
   {
     text: "框架",
@@ -19,6 +15,7 @@ const nav: DefaultTheme.NavItem[] = [
       { text: "VitePress", link: "/framework/VitePress/configure-algolia" },
       { text: "React", link: "/framework/React/1.React 中常见的 Hooks" },
       { text: "Nest", link: "/framework/Nest/1.Nest 基础概念" },
+      { text: "CSS", link: "/framework/CSS/attributes/Flex和inline-flex" },
     ],
   },
 ];
